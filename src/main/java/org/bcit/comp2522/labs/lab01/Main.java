ff0c99d3c84cf2cde06007102184a941b442a6c4
Simrat Kaur <simratkaur2@my.bcit.ca>
2023-01-05 17:41:10 -0800

    Printing a warm welcome to CST second semester.

---
+++ src/main/java/org/bcit/comp2522/labs/lab01/Main.java
@@ -0,0 +1,8 @@
+package org.bcit.comp2522.labs.lab01;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello CST Semester 2" +
+                "I am ready for you.");
+    }
+}
